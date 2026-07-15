7f253ddc593b9f125e97566aa1ce34e5f1aa4845
Carlos Silva <carlos110eduardo@gmail.com>
2026-07-15 11:10:12 -0300

    adicionado o codigo de lista simplemente encadeada

---
+++ lista-simplesmente-encadeada.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct SFila
+{
+    int dado;
+    struct SFila *next;
+} Fila;
+
+Fila *novo = NULL;
+Fila *fila = NULL;
+Fila *inicio = NULL;
+Fila *ultimo = NULL;
+
+void listarFila()
+{
+    ultimo = fila;
+    if (fila == NULL)
+    {
+        printf("fila esta vazia.\n");
+        return;
+    }
+    do
+    {
+        printf("dado: %d \n", ultimo->dado);
+        ultimo = ultimo->next;
+    } while (ultimo != NULL);
+    printf("fim da fila.\n");
+}
+
+void adicionarElementoNaFila(int informacao)
+{
+    ultimo = fila;
+    novo = (Fila *)malloc(sizeof(Fila));
+    if (!novo)
+    {
+        printf("memoria insuficiente.\n");
+        return;
+    }
+    novo->dado = informacao;
+    novo->next = NULL;
+    if (fila == NULL)
+    {
+        fila = novo;
+    }
+    else
+    {
+        while (ultimo->next != NULL)
+        {
+            ultimo = ultimo->next;
+        }
+        ultimo->next = novo;
+    }
+    printf("elemento adicionado.\n");
+}
+void removerElementoDaFila()
+{
+    inicio = fila;
+    if (fila == NULL)
+    {
+        printf("Fila vazia.\n");
+        return;
+    }
+    inicio = fila->next;
+    free(fila);
+    fila = inicio;
+    printf("Elemento removido.\n");
+}
+
+void menu()
+{
+    unsigned short int escolha = 0;
+    int valor = 0;
+    do
+    {
+        printf("1- adicionar elemento\n");
+        printf("2- remover elemento\n");
+        printf("3- listar elementos da fila\n");
+        printf("0-sair\n");
+        printf("escolha uma opcao: ");
+        scanf("%d", &escolha);
+        system("cls");
+        switch (escolha)
+        {
+
+        case 1:
+            printf("digite o valor a ser adicionado: ");
+            scanf("%d", &valor);
+            adicionarElementoNaFila(valor);
+            break;
+        case 2:
+            removerElementoDaFila();
+            break;
+        case 3:
+            listarFila();
+            break;
+        default:
+            printf("opcao invalida! tente outra: ");
+        }
+    } while (escolha != 0);
+}
+int main()
+{
+    //	adicionarElementoNaFila(1);
+    //	adicionarElementoNaFila(2);
+    //	adicionarElementoNaFila(3);
+    //	adicionarElementoNaFila(4);
+    //	adicionarElementoNaFila(5);
+    //	adicionarElementoNaFila(6);
+    //	listarFila();
+    //	removerElementoDaFila();
+    //	removerElementoDaFila();
+    //	listarFila();
+    menu();
+    system("pause");
+    return 0;
+}
